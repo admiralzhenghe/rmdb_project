2fd431571d4a236cc0346faca936827c79e9835e
ZHQ <herman.zheng@aya.yale.edu>
2021-12-25 22:53:33 -0500

    created apiOverview view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+# REST Framework
+from rest_framework import serializers
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 # Create your views here.
+@api_view(['GET'])
+def apiOverview(request):
+  api_urls = {
+    'User': '/user/'
+  }
+  return Response(api_urls)
+
