63fd93163814ae48ea28417b99f3de985d451ba1
ZHQ <heqzheng@gmail.com>
2022-01-18 12:21:36 -0500

    updated registration path

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('get-movies/', views.getMovies, name='get-movies'),
     # Django Rest Auth
     path('rest-auth/', include('dj_rest_auth.urls')),
-    path('rest-auth/register/', include('dj_rest_auth.registration.urls')),
+    path('rest-auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
