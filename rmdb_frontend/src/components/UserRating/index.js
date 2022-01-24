32ecc2a3af5bf7203bfc8fa1e38ee0c4fbeb18de
ZHQ <heqzheng@gmail.com>
2022-01-24 14:25:28 -0500

    fixed rating reset bug

--- rmdb_frontend/src/components/UserRating/index.js
+++ rmdb_frontend/src/components/UserRating/index.js
@@ -1,63 +1,38 @@
-import React, { useEffect, useRef, useState } from "react";
-// Context
-import { useAuthContext } from "../../context/AuthContext";
+import React, { useEffect, useRef } from "react";
 // Images
 import popcorn from "../../images/popcorn.svg";
 // Styles
 import { Wrapper } from "./UserRating.styles";
 
-export default function UserRating({ movie, movieLog }) {
-  const { tokens, user } = useAuthContext();
-  const [rating, setRating] = useState(movieLog.rating);
-
+export default function UserRating({ movieLog, setMovieLog }) {
   const popcornRatingEl = useRef(null);
-  const previousRating = useRef(rating);
+  const previousRating = useRef(movieLog.rating);
 
   const handleClick = (e) => {
-    let rating = e.currentTarget.closest("label").querySelector("input").value;
+    let rating = Number(
+      e.currentTarget.closest("label").querySelector("input").value
+    );
+    // If the user clicks on the same rating twice, reset the rating to zero
     if (rating === previousRating.current) {
-      setRating(0);
-      previousRating.current = null;
+      setMovieLog({ ...movieLog, rating: 0 });
+      previousRating.current = 0;
     } else {
       previousRating.current = rating;
-      setRating(Number(rating));
+      setMovieLog({ ...movieLog, rating });
     }
   };
 
-  // Update the number of visible popcorns based on the user's selection
+  // Update the number of visible popcorns based on the user's rating
   useEffect(() => {
-    for (let i = 0; i < 6; i++) {
-      popcornRatingEl.current.childNodes[i].children[1].classList.add(
-        "unselected"
-      );
-    }
-    for (let i = 0; i < rating; i++) {
+    popcornRatingEl.current.childNodes.forEach((child) => {
+      child.querySelector("img").classList.add("unselected");
+    });
+    for (let i = 0; i < movieLog.rating; i++) {
       popcornRatingEl.current.childNodes[i].children[1].classList.remove(
         "unselected"
       );
     }
-  }, [rating]);
-
-  // Update the popcorn ratings in the database
-  useEffect(() => {
-    if (rating === previousRating.current) return;
-
-    const API_URL = "http://127.0.0.1:8000/api/update-movie/";
-    fetch(`${API_URL}${movie.id}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${tokens.access}`,
-      },
-      body: JSON.stringify({
-        user: user.pk,
-        movieId: movie.id,
-        title: movie.title,
-        posterPath: movie.poster_path,
-        rating,
-      }),
-    });
-  }, [rating]);
+  }, [movieLog.rating]);
 
   return (
     <Wrapper>
