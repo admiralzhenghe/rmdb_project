0d95788988b290018a4947d49489802bf60ed8c3
ZHQ <heqzheng@gmail.com>
2022-02-07 15:11:06 -0500

    integrated React frontend with Django backend

--- rmdb_project/settings.py
+++ rmdb_project/settings.py
@@ -128,7 +128,9 @@ ROOT_URLCONF = 'rmdb_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'rmdb_frontend/build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -192,6 +194,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'rmdb_frontend/build/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
